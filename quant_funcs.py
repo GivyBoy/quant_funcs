61810ea69b51e660a4a0eae7d124975e5dcaa0fc
Anthony Givans <givansanthony702@gmail.com>
2022-05-22 16:05:34 -0400

    added cumulative returns (multiple) function

--- quant_funcs.py
+++ quant_funcs.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import yfinance as yf
 import matplotlib.pyplot as plt
@@ -33,12 +32,50 @@ class QuantFuncs:
 
         return df
 
+    def get_daily_cumulative_returns_multiple(self, data, weights):
+
+        returns = yf.download(data, start='2015-01-01', end='2022-05-20')['Adj Close']
+
+        rets = pd.DataFrame()
+        for i in returns.columns.values:
+            rets[f"{i}-rets"] = returns[f"{i}"].pct_change() + 1
+
+        port_pct = []
+
+        for i in range(1258, len(rets)):
+            sum_port = 0
+
+            for j, val in enumerate(rets.columns):
+
+                sum_port += (rets[val].iloc[i] * weights[j])
+
+                if i == 4:
+                    print(f"{j}: {sum_port}")
+
+            port_pct.append(sum_port)
+
+        cum_port = []
+
+        for i, val in enumerate(port_pct):
+
+            if i == 0:
+                cum_port.append(1)
+
+            else:
+                cum_port.append(cum_port[i - 1] * val)
+
+        data_port = {"Daily Cumulative Returns": cum_port}
+
+        cum_rets = pd.DataFrame(data_port)
+
+        cum_rets.plot()
+
+        return cum_rets
+
 
 if __name__ == "__main__":
-
     q = QuantFuncs()
 
     aapl = q.get_cumulative_returns('aapl', 1000)
 
     print(aapl)
-
